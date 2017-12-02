1d4234ea643fe6a96e4fd7ee39647265346dcf7f
Noah Verkaik <nowahamail@gmail.com>
2017-12-02 19:45:07 +0100

    Ok
    Ok

---
+++ Player/Player/Assets/Scripts/CamFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+
+    [Header("Camera Attributes")]
+    public float cameraSpeed = 0.01f;
+    [Header("Camera Bounds")]
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+    [Header("Required References")]
+    public Transform target;
+    Camera cam;
+
+    // Use this for initialization
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (target == null)
+            return;
+
+        transform.position = Vector3.Lerp(transform.position, new Vector3((target.position.x < xMax && target.position.x > xMin) ? target.position.x : transform.position.x, (target.position.y < yMax && target.position.y > yMin) ? target.position.y : transform.position.y, -10), cameraSpeed);
+    }
+}
