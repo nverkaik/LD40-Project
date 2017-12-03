d4a1a9fbe57c4654f079819eae240bffea67e38e
ChaosTheLegend <chaosthelegend@inbox.ru>
2017-12-03 23:59:57 +0600

    boss and enemies

---
+++ Enemies/Player/Assets/NecromancerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NecromancerController : MonoBehaviour {
+
+    public Transform Player;
+    public GameObject[] SpawnEnemies;
+    public GameObject arrow;
+    public Transform[] Spawnpoints;
+    public float DelayBetweenAttacks;
+    private float timer;
+    public float ArrowSpeed;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timer <= 0)
+        {
+            timer = DelayBetweenAttacks;
+            int attack = (int)Mathf.Floor(Random.value * 2);
+            if (attack == 2)
+            {
+                attack = 1;
+            }
+            StartAttack(attack);
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+	}
+    void StartAttack(int attacktype)
+    {
+        //summon skeletons
+        if (attacktype == 0)
+        {
+            foreach (Transform spawnpoint in Spawnpoints)
+            {
+                if (Random.Range(0,100) <= 25)
+                {
+                    int enemy = (int)Mathf.Floor(Random.value * 2);
+                    if (enemy == 2)
+                    {
+                        enemy = 1;
+                    }
+                    Instantiate(SpawnEnemies[enemy], spawnpoint.position, spawnpoint.rotation).GetComponent<EnemyHPControll>().Player = Player;
+                }
+
+
+            }
+           
+        }
+        //shoot bones
+        if (attacktype == 1)
+        {
+            for (int i = 0; i < (int)Mathf.Floor(Random.Range(7, 13)); i++)
+            {
+                Quaternion q = Quaternion.Euler(0f,0f,Random.Range(0, 360));
+                GameObject newarrow = Instantiate(arrow, transform.position, q);
+                newarrow.GetComponent<Rigidbody2D>().velocity = newarrow.transform.right * ArrowSpeed;
+                Destroy(newarrow, 4f);
+            }
+        }
+    }
+}
---
+++ Enemies/Player/Assets/Scripts/EnemyHPControll.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHPControll : MonoBehaviour {
+
+    public Transform Player;
+    public Slider EnemyHPSlider;
+    public int EnemyHP;
+    public int EnemyMaxHP;
+    public Transform canvas;
+
+    // Use this for initialization
+    void Start () {
+        EnemyHPSlider.value = EnemyHP;
+        EnemyHP = EnemyMaxHP;
+        EnemyHPSlider.maxValue = EnemyMaxHP;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        EnemyHPSlider.value = EnemyHP;
+        EnemyHPSlider.maxValue = EnemyMaxHP;
+        canvas.position = transform.position;
+        if (EnemyHP == EnemyMaxHP)
+        {
+            EnemyHPSlider.gameObject.SetActive(false);
+        }
+        else
+        {
+            EnemyHPSlider.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Contains("Player"))
+        {
+            EnemyHP -= other.gameObject.GetComponent<DamageControll>().damage;
+            if (other.tag.Contains("Arrow"))
+            {
+                other.tag = "Useless";
+                other.transform.parent = transform;
+                other.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+
+                //Destroy(other.gameObject);
+            }
+            
+            if (EnemyHP <=0)
+            {
+                Destroy(canvas.gameObject);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
